647e9b83672bf100dac27da32df39236cf5f0446
3zad <test106437@gmail.com>
2025-11-21 03:15:20 +0200

    website

--- tests/LambdaCalculusInterpreter.tests/Program.fs
+++ tests/LambdaCalculusInterpreter.tests/Program.fs
@@ -16,30 +16,30 @@ let main _ =
 
     // Fully-fledged example
     match execsFast Normal """
-        % NB! Running in applicative mode is many times faster!
+% NB! This is a comment
 
-        % Define some constants
-        ten = 10;
-        twenty = 20;
-        fifty = 50;
-        hundred = 100;
+% Define some constants
+ten = 10;
+twenty = 20;
+fifty = 50;
+hundred = 100;
         
-        % Some arithmetic
-        doubleTen = mul ten (succ ten); % 10 * 11 = 110
-        quadrupleTen = mul doubleTen (succ ten); % 110 * 11 = 1210 (will reduce later)
-        halfHundred = sub hundred fifty; % 100 - 50 = 50
+% Some arithmetic
+doubleTen = mul ten (succ ten); % 10 * 11 = 110
+quadrupleTen = mul doubleTen (succ ten); % 110 * 11 = 1210 (will reduce later)
+halfHundred = sub hundred fifty; % 100 - 50 = 50
 
-        % Nested arithmetic using constants
-        result1 = add doubleTen halfHundred; % 110 + 50 = 160
-        result2 = sub (mul ten twenty) (add fifty ten); % 200 - 60 = 140
+% Nested arithmetic using constants
+result1 = add doubleTen halfHundred; % 110 + 50 = 160
+result2 = sub (mul ten twenty) (add fifty ten); % 200 - 60 = 140
 
-        % Recursion
-        addRec = λ n m. cond (iszero n) m (addRec (sub n 1) (add m 1));
+% Recursion
+addRec = λ n m. cond (iszero n) m (addRec (sub n 1) (add m 1));
 
-        print (add 2 2);
+print (add 2 2);
 
-        main = fact( add result2 (add (add result1 result2) (addRec (succ ten) (3)) ) ); % (140 + (160 + 140) + (11 + 3))! =~ 7.2670e1101
-        print main;
+main = fact( add result2 (add (add result1 result2) (addRec (succ ten) (3)) ) ); % (140 + (160 + 140) + (11 + 3))! =~ 7.2670e1101
+print main;
 
     """ with
     | Result.Ok x -> printfn "%A" x
